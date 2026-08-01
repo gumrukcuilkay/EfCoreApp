c7651739e857b11420f72d0a203cdb9f1990f877
İlkay Gümrükcü <118602074+gumrukcuilkay@users.noreply.github.com>
2026-08-01 22:50:49 +0300

    Add files via upload

---
+++ Migrations/20250726200502_AddBaslamaTarihiToOgretmen.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace efcoreApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBaslamaTarihiToOgretmen : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BaşlamaTarihi",
+                table: "Ogretmenler",
+                newName: "BaslamaTarihi");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BaslamaTarihi",
+                table: "Ogretmenler",
+                newName: "BaşlamaTarihi");
+        }
+    }
+}
